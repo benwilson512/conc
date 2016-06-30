c67158babfddd53222706a303b2ada1d76a0f003
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-06-30 21:40:41 +0200

    Tuples!

--- lib/conc.ex
+++ lib/conc.ex
@@ -121,7 +121,8 @@ defmodule Conc do
   def from_list([]), do: {}
   def from_list([x]), do: {x}
   def from_list([head|tail]) do
-    rebalance({{head}, from_list(tail)})
+    {{head}, from_list(tail)}
+    # |> rebalance
   end
 
   ## Fun Stuff
@@ -136,7 +137,7 @@ defmodule Conc do
   def map_reduce(xs, id, mapping_fun, reducing_fun)
 
   def map_reduce({}, id, _, _), do: id
-  def map_reduce({x}, _, mapping_fun, _), do: mapping_fun.(x)
+  def map_reduce({x}, _, mapping_fun, _), do: [mapping_fun.(x)]
   def map_reduce(xs, id, mapping_fun, reducing_fun) do
     IO.puts "map_reduce called with #{inspect xs}"
     split(xs, &reducing_fun.(map_reduce(&1, id, mapping_fun, reducing_fun), map_reduce(&2, id, mapping_fun, reducing_fun)))
